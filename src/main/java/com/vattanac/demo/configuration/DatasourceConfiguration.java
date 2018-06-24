f5478792c93fe0fe142d3b5ddd41cdd2490c8d4c
vattanac <vattanacsim99@gmail.com>
2018-06-24 14:52:03 +0700

    work with H2 database

--- src/main/java/com/vattanac/demo/configuration/DatasourceConfiguration.java
+++ src/main/java/com/vattanac/demo/configuration/DatasourceConfiguration.java
@@ -2,14 +2,18 @@ package com.vattanac.demo.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DatasourceConfiguration {
 
-    @Bean
+    @Bean("dataSource")
+    @Profile("my-psq")
     public DataSource dataSource(){
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
 
@@ -18,5 +22,17 @@ public class DatasourceConfiguration {
         driverManagerDataSource.setUsername("postgres");
         driverManagerDataSource.setPassword("1998V");
         return driverManagerDataSource;
+    }
+
+    @Bean("dataSource")
+    @Profile("my-h2")
+    public DataSource inMemoryDB(){
+        EmbeddedDatabaseBuilder databaseBuilder = new EmbeddedDatabaseBuilder();
+
+        databaseBuilder.setType(EmbeddedDatabaseType.H2);
+        databaseBuilder.addScript("db/schema.sql");
+        databaseBuilder.addScript("db/data.sql");
+
+        return  databaseBuilder.build();
     }
 }
